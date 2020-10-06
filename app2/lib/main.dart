91c0634d34e4f791cc32fdbceafb20812191f0d4
Denis Stark <Denis Stark>
2020-10-06 14:24:43 -0600

    Add search panel

--- app2/lib/main.dart
+++ app2/lib/main.dart
@@ -6,6 +6,8 @@ import 'package:country_code_picker/country_code_picker.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 // Sliding panel for search filters and camera
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+// Input chips
+import 'package:flutter_chips_input/flutter_chips_input.dart';
 
 void main() {
   runApp(MyApp());
@@ -197,6 +199,396 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 enum MainViewToggle { map, list }
+enum FilterType { author, title, genre, language, location, whish, contacts }
+
+class Filter {
+  final FilterType type;
+  String value;
+  bool state = true;
+  Filter({@required this.type, this.value, this.state});
+}
+
+class TitleChipsWidget extends StatefulWidget {
+  final List<Filter> filters;
+
+  TitleChipsWidget({Key key, this.filters}) : super(key: key);
+
+  @override
+  _TitleChipsState createState() => _TitleChipsState(filters);
+}
+
+class _TitleChipsState extends State<TitleChipsWidget> {
+  // TODo: Replace mock with real values
+  static List<String> titles = <String>[
+    'Good to great',
+    'Great by choice',
+    '8 habits highly effective people',
+    'Lord of the ring',
+    'Jim Collins',
+    'Max Fry'
+  ];
+
+  static List<Filter> findTitleSugestions(String query) {
+    if (query.length != 0) {
+      var lowercaseQuery = query.toLowerCase();
+      return titles
+          .where((title) {
+            return title.toLowerCase().contains(query.toLowerCase());
+          })
+          .map((g) => Filter(type: FilterType.title, value: g))
+          .toList(growable: false)
+            ..sort((a, b) => a.value
+                .toLowerCase()
+                .indexOf(lowercaseQuery)
+                .compareTo(b.value.toLowerCase().indexOf(lowercaseQuery)));
+    } else {
+      return const <Filter>[];
+    }
+  }
+
+  static Widget titleChipBuilder(
+      BuildContext context, ChipsInputState<Filter> state, Filter title) {
+    return InputChip(
+      key: ObjectKey(title),
+      label: Text(title.value),
+      // TODO: Put book icon here
+      // avatar: CircleAvatar(),
+      onDeleted: () => state.deleteChip(title),
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    );
+  }
+
+  static Widget titleSugestionBuilder(
+      BuildContext context, ChipsInputState<Filter> state, Filter title) {
+    return ListTile(
+      key: ObjectKey(title),
+      // TODO: Add leading avatar with book
+      // leading: CircleAvatar(),
+      title: Text(title.value),
+      subtitle: Text(title.value),
+      onTap: () => state.selectSuggestion(title),
+    );
+  }
+
+  // List of genre filters
+  List<Filter> filters;
+
+  _TitleChipsState(this.filters);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChipsInput(
+      initialValue: filters,
+      decoration: InputDecoration(
+        labelText: "Title / Author",
+      ),
+      maxChips: 5,
+      findSuggestions: findTitleSugestions,
+      onChanged: (data) {
+        print(data);
+      },
+      chipBuilder: titleChipBuilder,
+      suggestionBuilder: titleSugestionBuilder,
+    );
+  }
+}
+
+class GenreChipsWidget extends StatefulWidget {
+  final List<Filter> filters;
+
+  GenreChipsWidget({Key key, this.filters}) : super(key: key);
+
+  @override
+  _GenreChipsState createState() => _GenreChipsState(filters);
+}
+
+class _GenreChipsState extends State<GenreChipsWidget> {
+  // TODo: Replace mock with real values
+  static List<String> genres = <String>[
+    'Fiction',
+    'Non-fiction',
+    'Fantasy',
+    'Novel'
+  ];
+
+  static List<Filter> findGenreSugestions(String query) {
+    if (query.length != 0) {
+      var lowercaseQuery = query.toLowerCase();
+      return genres
+          .where((genre) {
+            return genre.toLowerCase().contains(query.toLowerCase());
+          })
+          .map((g) => Filter(type: FilterType.genre, value: g))
+          .toList(growable: false)
+            ..sort((a, b) => a.value
+                .toLowerCase()
+                .indexOf(lowercaseQuery)
+                .compareTo(b.value.toLowerCase().indexOf(lowercaseQuery)));
+    } else {
+      return const <Filter>[];
+    }
+  }
+
+  static Widget genreChipBuilder(
+      BuildContext context, ChipsInputState<Filter> state, Filter genre) {
+    return InputChip(
+      key: ObjectKey(genre),
+      label: Text(genre.value),
+      // TODO: Put book icon here
+      // avatar: CircleAvatar(),
+      onDeleted: () => state.deleteChip(genre),
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    );
+  }
+
+  static Widget genreSugestionBuilder(
+      BuildContext context, ChipsInputState<Filter> state, Filter genre) {
+    return ListTile(
+      key: ObjectKey(genre),
+      // TODO: Add leading avatar with book
+      // leading: CircleAvatar(),
+      title: Text(genre.value),
+      subtitle: Text(genre.value),
+      onTap: () => state.selectSuggestion(genre),
+    );
+  }
+
+  // List of genre filters
+  List<Filter> filters;
+
+  _GenreChipsState(this.filters);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChipsInput(
+      initialValue: filters,
+      decoration: InputDecoration(
+        labelText: "Genre",
+      ),
+      maxChips: 5,
+      findSuggestions: findGenreSugestions,
+      onChanged: (data) {
+        print(data);
+      },
+      chipBuilder: genreChipBuilder,
+      suggestionBuilder: genreSugestionBuilder,
+    );
+  }
+}
+
+class PlaceChipsWidget extends StatefulWidget {
+  final List<Filter> filters;
+
+  PlaceChipsWidget({Key key, this.filters}) : super(key: key);
+
+  @override
+  _PlaceChipsState createState() => _PlaceChipsState(filters);
+}
+
+class _PlaceChipsState extends State<PlaceChipsWidget> {
+  // TODo: Replace mock with real values
+  static List<String> places = <String>[
+    'Bukvoed',
+    'Itaka',
+    'Denis Stark',
+    'Ernesto'
+  ];
+
+  static List<Filter> findPlaceSugestions(String query) {
+    if (query.length != 0) {
+      var lowercaseQuery = query.toLowerCase();
+      return places
+          .where((place) {
+            return place.toLowerCase().contains(query.toLowerCase());
+          })
+          .map((g) => Filter(type: FilterType.language, value: g))
+          .toList(growable: false)
+            ..sort((a, b) => a.value
+                .toLowerCase()
+                .indexOf(lowercaseQuery)
+                .compareTo(b.value.toLowerCase().indexOf(lowercaseQuery)));
+    } else {
+      return const <Filter>[];
+    }
+  }
+
+  static Widget placeChipBuilder(
+      BuildContext context, ChipsInputState<Filter> state, Filter place) {
+    return InputChip(
+      key: ObjectKey(place),
+      label: Text(place.value),
+      // TODO: Put book icon here
+      // avatar: CircleAvatar(),
+      onDeleted: () => state.deleteChip(place),
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    );
+  }
+
+  static Widget placeSugestionBuilder(
+      BuildContext context, ChipsInputState<Filter> state, Filter place) {
+    return ListTile(
+      key: ObjectKey(place),
+      // TODO: Add leading avatar with book
+      // leading: CircleAvatar(),
+      title: Text(place.value),
+      subtitle: Text(place.value),
+      onTap: () => state.selectSuggestion(place),
+    );
+  }
+
+  // List of genre filters
+  List<Filter> filters;
+
+  _PlaceChipsState(this.filters);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChipsInput(
+      initialValue: filters,
+      decoration: InputDecoration(
+        labelText: "Place / Contact",
+      ),
+      maxChips: 5,
+      findSuggestions: findPlaceSugestions,
+      onChanged: (data) {
+        print(data);
+      },
+      chipBuilder: placeChipBuilder,
+      suggestionBuilder: placeSugestionBuilder,
+    );
+  }
+}
+
+class LanguageChipsWidget extends StatefulWidget {
+  final List<Filter> filters;
+
+  LanguageChipsWidget({Key key, this.filters}) : super(key: key);
+
+  @override
+  _LanguageChipsState createState() => _LanguageChipsState(filters);
+}
+
+class _LanguageChipsState extends State<LanguageChipsWidget> {
+  // TODo: Replace mock with real values
+  static List<String> languages = <String>['DEU', 'ENG', 'FRA', 'RUS'];
+
+  static List<Filter> findLanguageSugestions(String query) {
+    if (query.length != 0) {
+      var lowercaseQuery = query.toLowerCase();
+      return languages
+          .where((genre) {
+            return genre.toLowerCase().contains(query.toLowerCase());
+          })
+          .map((g) => Filter(type: FilterType.language, value: g))
+          .toList(growable: false)
+            ..sort((a, b) => a.value
+                .toLowerCase()
+                .indexOf(lowercaseQuery)
+                .compareTo(b.value.toLowerCase().indexOf(lowercaseQuery)));
+    } else {
+      return const <Filter>[];
+    }
+  }
+
+  static Widget languageChipBuilder(
+      BuildContext context, ChipsInputState<Filter> state, Filter language) {
+    return InputChip(
+      key: ObjectKey(language),
+      label: Text(language.value),
+      // TODO: Put book icon here
+      // avatar: CircleAvatar(),
+      onDeleted: () => state.deleteChip(language),
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    );
+  }
+
+  static Widget languageSugestionBuilder(
+      BuildContext context, ChipsInputState<Filter> state, Filter language) {
+    return ListTile(
+      key: ObjectKey(language),
+      // TODO: Add leading avatar with book
+      // leading: CircleAvatar(),
+      title: Text(language.value),
+      subtitle: Text(language.value),
+      onTap: () => state.selectSuggestion(language),
+    );
+  }
+
+  // List of genre filters
+  List<Filter> filters;
+
+  _LanguageChipsState(this.filters);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChipsInput(
+      initialValue: filters,
+      decoration: InputDecoration(
+        labelText: "Language",
+      ),
+      maxChips: 5,
+      findSuggestions: findLanguageSugestions,
+      onChanged: (data) {
+        print(data);
+      },
+      chipBuilder: languageChipBuilder,
+      suggestionBuilder: languageSugestionBuilder,
+    );
+  }
+}
+
+class SearchPanel extends StatefulWidget {
+  SearchPanel({Key key, this.filters, this.collapsed}) : super(key: key);
+
+  final List<Filter> filters;
+  final bool collapsed;
+
+  @override
+  _SearchPanelState createState() => _SearchPanelState(filters, collapsed);
+}
+
+class _SearchPanelState extends State<SearchPanel> {
+  final List<Filter> filters;
+  bool collapsed;
+  bool onlyMine = false;
+  bool onlyWishlist = false;
+
+  _SearchPanelState(this.filters, this.collapsed);
+
+  @override
+  Widget build(BuildContext context) {
+    if (collapsed) {
+      return GenreChipsWidget(filters: filters);
+    } else {
+      return Column(children: [
+        TitleChipsWidget(filters: filters),
+        GenreChipsWidget(filters: filters),
+        PlaceChipsWidget(filters: filters),
+        LanguageChipsWidget(filters: filters),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text('Only my places and contacts'),
+          Switch(
+              value: onlyMine,
+              onChanged: (value) {
+                setState(() {
+                  onlyMine = value;
+                });
+              }),
+        ]),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text('Only books from my wishlist'),
+          Switch(
+              value: onlyWishlist,
+              onChanged: (value) {
+                setState(() {
+                  onlyWishlist = value;
+                });
+              }),
+        ])
+      ]);
+    }
+  }
+}
 
 class MainPage extends StatefulWidget {
   MainPage({Key key}) : super(key: key);
@@ -207,28 +599,18 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   MainViewToggle view = MainViewToggle.map;
+  List<Filter> filters = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SlidingUpPanel(
-      minHeight: 50,
-      maxHeight: 200,
+      minHeight: 90,
+      maxHeight: 390,
       // Figma: Closed Search panel
-      collapsed: Container(
-          margin: EdgeInsets.only(right: 10.0, left: 10.0),
-          child: Wrap(
-            spacing: 5.0,
-            children: <Widget>[
-              // TODO: Implement chip filters
-              InputChip(label: Text('RU'), selected: true),
-              InputChip(label: Text('Non-fiction'), selected: true),
-            ],
-          )),
+      //collapsed: SearchPanel(filters: filters, collapsed: true),
       // Figma: Open search panel
-      panel: Center(
-        child: Text("Search details"),
-      ),
+      panel: SearchPanel(filters: filters, collapsed: false),
       body: MapWidget(),
     ));
   }
