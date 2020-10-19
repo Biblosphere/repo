fd2f17048869af47ce6f7cd7e0a235dec9489bd2
Denis Stark <Denis Stark>
2020-10-19 08:26:48 -0600

    Subscription screen added

--- app2/lib/main.dart
+++ app2/lib/main.dart
@@ -24,6 +24,8 @@ import 'package:camera/camera.dart';
 import 'package:path_provider/path_provider.dart';
 // Compare objects by content
 import 'package:equatable/equatable.dart';
+// Plugin for subscriptions
+import 'package:purchases_flutter/purchases_flutter.dart';
 
 List<CameraDescription> cameras;
 
@@ -86,29 +88,34 @@ class _MyAppState extends State<MyApp> {
 
 enum LoginStatus { unauthorized, phoneEntered, phoneConfirmed, subscribed }
 
+enum SubscriptionPlan { monthly, anual, business }
+
 class LoginState extends Equatable {
   final LoginStatus status;
   final String phone;
   final String code;
+  final SubscriptionPlan plan;
 
   @override
-  List<Object> get props => [status, phone, code];
+  List<Object> get props => [status, phone, code, plan];
 
   const LoginState(
       {this.status = LoginStatus.unauthorized,
       this.phone = '',
-      this.code = ''});
+      this.code = '',
+      this.plan = SubscriptionPlan.anual});
 
   LoginState copyWith({
     String phone,
     String code,
     LoginStatus status,
+    SubscriptionPlan plan,
   }) {
     return LoginState(
-      status: status ?? this.status,
-      phone: phone ?? this.phone,
-      code: code ?? this.code,
-    );
+        status: status ?? this.status,
+        phone: phone ?? this.phone,
+        code: code ?? this.code,
+        plan: plan ?? this.plan);
   }
 }
 
@@ -123,6 +130,14 @@ class LoginCubit extends Cubit<LoginState> {
   }
 
   void phoneConfirmed(String value) {
+    emit(state.copyWith(code: value, status: LoginStatus.phoneConfirmed));
+  }
+
+  void planSelected(SubscriptionPlan value) {
+    emit(state.copyWith(plan: value));
+  }
+
+  void phoneSubscribed(String value) {
     emit(state.copyWith(
       code: value,
       status: LoginStatus.subscribed,
@@ -153,7 +168,7 @@ class _LoginPageState extends State<LoginPage> {
                 Expanded(flex: 1, child: Center(child: Text('BIBLIO'))),
                 // Input fields (Phone or Confirmation Code)
                 Expanded(
-                    flex: 1,
+                    flex: 4,
                     child: Container(
                         margin: EdgeInsets.only(left: 40.0, right: 40.0),
                         child: Column(
@@ -193,7 +208,7 @@ class _LoginPageState extends State<LoginPage> {
 
                 // Button (Sign-In or Confirm)
                 Expanded(
-                  flex: 0,
+                  flex: 1,
                   child:
                       // Figma: Log In
                       RaisedButton(
@@ -207,7 +222,7 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 // Confirm PP & TS
                 Expanded(
-                    flex: 1,
+                    flex: 2,
                     child: Container(
                         margin: EdgeInsets.only(left: 20.0, right: 20.0),
                         child: Column(
@@ -252,7 +267,7 @@ class _LoginPageState extends State<LoginPage> {
                 Expanded(flex: 1, child: Center(child: Text('BIBLIO'))),
                 // Input fields (Phone or Confirmation Code)
                 Expanded(
-                    flex: 1,
+                    flex: 4,
                     child: Container(
                         margin: EdgeInsets.only(left: 40.0, right: 40.0),
                         child: Column(
@@ -279,7 +294,7 @@ class _LoginPageState extends State<LoginPage> {
 
                 // Button (Sign-In or Confirm)
                 Expanded(
-                  flex: 0,
+                  flex: 1,
                   child: RaisedButton(
                       onPressed: () {
                         // TODO: Use actual code from text field or AUTO for Android
@@ -287,6 +302,7 @@ class _LoginPageState extends State<LoginPage> {
                       },
                       child: Text('Confirm code')),
                 ),
+                Expanded(flex: 2, child: Container()),
                 const Spacer()
               ]);
         } else {
@@ -297,47 +313,227 @@ class _LoginPageState extends State<LoginPage> {
                 const Spacer(),
                 Expanded(flex: 1, child: Center(child: Text('BIBLIO'))),
                 // Input fields (Phone or Confirmation Code)
-                Expanded(
-                    flex: 1,
-                    child: Container(
-                        margin: EdgeInsets.only(left: 40.0, right: 40.0),
-                        child: Column(
-                          children: [
-                            // Figma: Country Code
-                            Container(
-                                alignment: Alignment.centerRight,
-                                child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      Text('Resend in 20 sec'),
-                                    ])),
-
-                            // Figma: Confirmation code
-                            Container(
-                                child: Row(children: [
-                              Text('Code from SMS:'),
-                              Expanded(
-                                  child: TextField(
-                                      keyboardType: TextInputType.number))
-                            ])),
-                          ],
-                        ))),
+                Expanded(flex: 4, child: upgradeWidget()),
 
                 // Button (Sign-In or Confirm)
                 Expanded(
-                  flex: 0,
+                  flex: 1,
                   child: RaisedButton(
                       onPressed: () {
                         // TODO: Use actual code from text field or AUTO for Android
-                        context.bloc<LoginCubit>().phoneConfirmed('555');
+                        context.bloc<LoginCubit>().phoneSubscribed('1');
                       },
-                      child: Text('Confirm code')),
+                      child: Text('Subscribe')),
                 ),
+                // Confirm PP & TS
+                Expanded(
+                    flex: 2,
+                    child: Container(
+                        margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Figma: Privacy Policy
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Checkbox(
+                                        value: _agreeToPP,
+                                        onChanged: (value) {
+                                          setState(() {
+                                            _agreeToPP = value;
+                                          });
+                                        }),
+                                    Text('Agree to Privacy Policy'),
+                                  ]),
+
+                              // Figma: Terms of service
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Checkbox(
+                                        value: _agreeToTS,
+                                        onChanged: (value) {
+                                          setState(() {
+                                            _agreeToTS = value;
+                                          });
+                                        }),
+                                    Text('Agree to Privacy Policy'),
+                                  ]),
+                            ]))),
                 const Spacer()
               ]);
         }
       }),
     ));
+  }
+
+  Widget upgradeWidget() {
+    return BlocBuilder<LoginCubit, LoginState>(builder: (context, login) {
+      return Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+            // Row with three plan options to choose
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              //isSelected: planOptions,
+              children: <Widget>[
+                // Monthly option
+                productWidget(SubscriptionPlan.anual),
+                // Annual option
+                productWidget(SubscriptionPlan.monthly),
+                // Patron option
+                productWidget(SubscriptionPlan.business),
+              ],
+            ),
+            // Information about paid plan
+            productDescription(login.plan),
+            disclaimer(),
+          ]));
+    });
+  }
+
+  Widget disclaimer() {
+    String platform = Theme.of(context).platform == TargetPlatform.iOS
+        ? 'iTunes'
+        : 'Google Play';
+
+    return Container(
+        margin: EdgeInsets.only(top: 10.0),
+        child: Text(
+            'Subscription will be charged to your $platform account on confirmation. Subscriptions will automatically renew unless canceled within 24-hours before the end of the current period. You can cancel anytime with your $platform account settings. Any unused portion of a free trial will be forfeited if you purchase a subscription.',
+            style: Theme.of(context).textTheme.bodyText1));
+  }
+
+  Widget productDescription(SubscriptionPlan plan) {
+    switch (plan) {
+      case SubscriptionPlan.anual:
+        return Text('Enjoy annual plan! Save \$12',
+            style: Theme.of(context).textTheme.subtitle2);
+        break;
+      case SubscriptionPlan.monthly:
+        return Text('Enjoy monfly plan!',
+            style: Theme.of(context).textTheme.subtitle2);
+        break;
+      case SubscriptionPlan.anual:
+        return Text(
+            'Improve online presence of your bookstore with business plan.',
+            style: Theme.of(context).textTheme.subtitle2);
+        break;
+      default:
+        return Container();
+    }
+  }
+
+  // TODO: Get real data of packages from purchases plugin
+
+  Map<SubscriptionPlan, dynamic> plans = {
+    SubscriptionPlan.monthly: {
+      'title': 'Monthly',
+      'monthly': '\$2.00',
+      'price': '\$2.00',
+      'period': 'per month'
+    },
+    SubscriptionPlan.anual: {
+      'title': 'Annual',
+      'monthly': '\$1.00',
+      'price': '\$12.00',
+      'period': 'per year'
+    },
+    SubscriptionPlan.business: {
+      'title': 'Business',
+      'monthly': '\$50.00',
+      'price': '\$50.00',
+      'period': 'per month'
+    }
+  };
+
+/*
+    if (plan == SubscriptionPlan.monthly)
+      title = 'Monthly';
+    else if (plan == SubscriptionPlan.anual)
+      title = 'Annual';
+    else if (plan == SubscriptionPlan.business) title = 'Business';
+*/
+
+  Widget productWidget(SubscriptionPlan plan) {
+/*
+    // Get rid of "(Biblosphere)" in the title of Google Play products
+    String title = package.product.title.contains('(')
+        ? package.product.title.split('(')[0]
+        : package.product.title;
+
+    // Only Annual and monthly psubscriptions supported
+    String monthlyPrice = package.packageType == PackageType.annual
+        ? package.product.currencyCode +
+            ' ' +
+            (package.product.price / 12.0).toStringAsFixed(2)
+        : package.product.priceString;
+*/
+    return BlocBuilder<LoginCubit, LoginState>(builder: (context, login) {
+      return Expanded(
+          child: GestureDetector(
+              onTap: () {
+                context.bloc<LoginCubit>().planSelected(plan);
+              },
+              child: Container(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      // Highlight user choice
+                      decoration: plan == login.plan
+                          ? BoxDecoration(
+                              border: Border.all(
+                                color: Colors.red,
+                              ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5.0)),
+                              color: Colors.blue)
+                          : BoxDecoration(),
+                      child: Column(children: <Widget>[
+                        //Container(child: Text(package.packageType.toString())),
+                        Container(
+                            child: Text(plans[plan]['title'],
+                                style: Theme.of(context).textTheme.headline6)),
+                        // Show per month price uness it's annual plan and it's choosen
+                        login.plan != SubscriptionPlan.anual ||
+                                plan != SubscriptionPlan.anual
+                            ? Container(
+                                padding: EdgeInsets.only(top: 5.0),
+                                child: Text(plans[plan]['monthly'],
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2))
+                            : Container(),
+                        login.plan != SubscriptionPlan.anual ||
+                                plan != SubscriptionPlan.anual
+                            ? Container(
+                                child: Text('per month',
+                                    style:
+                                        Theme.of(context).textTheme.bodyText1))
+                            : Container(),
+                        plan == SubscriptionPlan.anual &&
+                                login.plan == SubscriptionPlan.anual
+                            ? Container(
+                                padding: EdgeInsets.only(top: 5.0),
+                                child: Text(plans[plan]['price'],
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2))
+                            : Container(),
+                        plan == SubscriptionPlan.anual &&
+                                login.plan == SubscriptionPlan.anual
+                            ? Container(
+                                child: Text(plans[plan]['period'],
+                                    style:
+                                        Theme.of(context).textTheme.bodyText1))
+                            : Container(),
+                      ])),
+                ],
+              ))));
+    });
   }
 }
 
